f164a7e846475f519df8b5781621d29aa3879081
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-07-10 19:29:34 +0800

    _

---
+++ src/utils/constant.py
@@ -0,0 +1,8 @@
+mark = (
+    "Heart Rate", \
+    "Respiratory Rate", \
+    "Non Invasive Blood Pressure systolic", \
+    "Non Invasive Blood Pressure diastolic", \
+    "O2 saturation pulseoxymetry", \
+    "Temperature Celsius"
+)
