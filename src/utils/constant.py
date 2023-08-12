39d1438b78e0880eebe1afa48fae725d1d2b50c6
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-08-13 00:32:22 +0800

    整理架構

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -3,6 +3,6 @@ mark = (
     "Respiratory Rate", \
     "Non Invasive Blood Pressure systolic", \
     "Non Invasive Blood Pressure diastolic", \
-    "O2 saturation pulseoxymetry", \
-    "Temperature Celsius"
+    "O2 saturation pulseoxymetry"#, \
+    #"Temperature Celsius"
 )
