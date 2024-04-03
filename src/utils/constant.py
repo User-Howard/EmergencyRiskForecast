be04d9cd3cb5646167da3aecfa0293dcf447cc0e
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-04-03 13:39:49 +0800

    刪除舊檔案

--- src/utils/constant.py
+++
@@ -1,8 +0,0 @@
-mark = (
-    "Heart Rate", \
-    "Respiratory Rate", \
-    "Non Invasive Blood Pressure systolic", \
-    "Non Invasive Blood Pressure diastolic", \
-    "O2 saturation pulseoxymetry", \
-    "Temperature Celsius"
-)
